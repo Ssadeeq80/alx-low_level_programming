4a855f12886c3cd2ef37bfde84b67c67121d1105
Ssadeeq80 <ssadeeq80@gmail.com>
2023-02-16 19:45:05 -0800

    Hello Puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main: the main function is to print the line of code using puts
+ *
+ * Return: 0
+ */
+int main(void)
+{
+		puts("\"Programming is like building a multilingual puzzle");
+		return (0);
+}
