00438d4b051d1f050753140c62e07e9e8185f18d
Ssadeeq80 <ssadeeq80@gmail.com>
2023-02-24 12:15:15 -0800

    Multiple mains

---
+++ 0x03-debugging/positive_or_negative.c
@@ -0,0 +1,13 @@
+/**
+ * positive_or_negative - Computes the absolute value of an integer
+ * @n: The integer to compute absolute value
+ *
+ * Return:  Absolute value of @n
+ */
+int positive_or_negative(int n)
+{
+	if (n < 0)
+		n *= -1;
+
+	return (n);
+}
