53184cd9866f3f2407cb762b6019962efa6594cf
Ssadeeq80 <ssadeeq80@gmail.com>
2023-02-27 11:20:19 -0800

    0.98 Battery st.

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,10 @@
+/**
+ * reset_to_98 - Updates the value of int of passed pointer
+ *					to 98
+ *
+ * @n: Pointer to an int
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,5 @@
+#ifndef MAIN_HEADER_FILE
+#define MAIN_HEADER_FILE
+	int _putchar(char c);
+	void reset_to_98(int *n);
+#endif
